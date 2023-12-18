2590e9a1a4d6d33d11c370744d9e24956a47c433
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:27:28 +0200

    Adding code examples/depop_curl.sh

---
+++ code examples/depop_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.depop.com/category/womens/bottoms/?categorypath=womens&categorypath=bottoms"}'
